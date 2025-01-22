673b6bfcb69300c733bf0e98f3458b56e9b89f3d
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-01-22 16:52:08 +0100

    First commit

---
+++ src/assets/assets.tsx
@@ -0,0 +1,32 @@
+import scissors from "./Scissors.png"
+import shave from "./Shaving-razor.png"
+import styling from "./styling.png"
+import trim from "./Trim.png"
+
+
+
+export const assets = {
+    scissors,
+    shave,
+    styling,
+    trim
+} 
+
+export const services = [
+    {
+        service: "HAIRCUT",
+        image: scissors
+    },
+    {
+        service: "SHAVING",
+        image: shave
+    },
+    {
+        service: "STYLING",
+        image: styling
+    },
+    {
+        service: "TRIMMING",
+        image: trim
+    }
+]
