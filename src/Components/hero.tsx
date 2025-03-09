81845c1edc2c7e42f3f1a811e1fa3eff2a59e49c
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-03-09 23:04:05 +0100

    Update

--- src/Components/hero.tsx
+++ src/Components/hero.tsx
@@ -1,5 +1,6 @@
-import { useNavigate } from "react-router-dom"
-import bgImage from "/banner.jpg"
+import { useNavigate } from "react-router-dom";
+import bgImage from "./Banner.jpg"
+
 
 const Hero: React.FC = () => {
 
@@ -16,7 +17,7 @@ const Hero: React.FC = () => {
                 <p className="text-white text-4xl md:text-6xl font-bold"> STYLE</p>
                 <hr className="w-1/6 h-0.5 my-5" />
                 <p className="text-sm md:text-lg text-white leading-3 font-extralight">Come by for a clean haircut, shave <br className="hidden md:fles" />and feel like a gentleman.</p>
-                <button onClick={() => {navigate("/booking"); scrollTo(0,0)}} className="border-2 border-yellow-700 text-xl text-white font-bold px-8 py-4 mt-5 hover:bg-yellow-700 transition-all duration-500">Book an appointment</button>
+                <button onClick={() => { navigate("/booking"); scrollTo(0, 0) }} className="border-2 border-yellow-700 text-xl text-white font-bold px-8 py-4 mt-5 hover:bg-yellow-700 transition-all duration-500">Book an appointment</button>
             </div>
 
         </section>
